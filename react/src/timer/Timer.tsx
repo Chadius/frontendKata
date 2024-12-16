246603ce7a8d8f689dedac6d53814128aa1325cf
Chad Serrant <therealchadius@gmail.com>
2024-12-16 09:41:24 -0500

    feat: useEffect to fix Timer render bugs.

--- react/src/timer/Timer.tsx
+++ react/src/timer/Timer.tsx
@@ -7,12 +7,20 @@ import {
     TimerState,
     updateTimeElapsed
 } from "./timerState.ts";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 
 const formatTimeElapsed = (timeElapsed: number): string => `${(timeElapsed / 1000).toFixed(1)}s`;
 
 const Timer = () => {
     const [timerState, setTimerState] = useState<TimerState>(defaultTimerState())
+
+    useEffect(() => {
+        const renderTime = () => {
+            setTimerState({...updateTimeElapsed(timerState)})
+        };
+        const tickId = setTimeout(renderTime, 50);
+        return () => clearTimeout(tickId);
+    }, [timerState]);
 
     const resetTimerOnClick = () => {
         setTimerState({
@@ -24,11 +32,6 @@ const Timer = () => {
         const duration = Number(e.target.value);
         setTimerState({...setTimerDuration(timerState, duration)})
     }
-
-    const renderTime = () => {
-        setTimerState({...updateTimeElapsed(timerState)})
-    };
-    setInterval(renderTime, 50);
 
     return (
         <>
