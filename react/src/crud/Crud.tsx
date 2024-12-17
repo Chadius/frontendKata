6a618c7263f4eb5e8f146addd313b3b634f70952
Chad Serrant <therealchadius@gmail.com>
2024-12-17 11:20:57 -0500

    feat: Add simple layout to CRUD page.

--- react/src/crud/Crud.tsx
+++ react/src/crud/Crud.tsx
@@ -1,11 +1,13 @@
 import '../App.css'
+import './crud.css'
 import {
     addNewRecord,
     CrudState,
     deleteRecord,
     filterRecordsBySurname,
     newCrudState,
-    selectFilteredRecord, updateRecord
+    selectFilteredRecord,
+    updateRecord
 } from "./crud.logic.ts";
 import {ChangeEvent, useState} from "react";
 
@@ -78,30 +80,41 @@ function Crud() {
     return (
         <article aria-label="crud">
             <h1>Crud</h1>
-            <label htmlFor="filter">Filter prefix:</label>
-            <input aria-label={"filter"} id="filter" name="filter" type={"text"} defaultValue={crudState.filter}
-                   onChange={onChangeFilter}/>
-
-            <select aria-label={"name-list"} id="name-list" name="name-list"
-                    size={4}
-                    onChange={onChangeSelectNameFromCrudList}
-            >
-                {generateSelectOptionDataFromCrudList(crudState)}
-            </select>
-
-            <label htmlFor="given-name">Name:</label>
-            <input aria-label={"given-name"} id="given-name" name="given-name" type={"text"}
-                   defaultValue={givenName}
-                   onChange={onChangeGivenName}/>
-
-            <label htmlFor="surname">Surname:</label>
-            <input aria-label={"surname"} id="surname" name="surname" type={"text"}
-                   defaultValue={surname}
-                   onChange={onChangeSurname}/>
-
-            <button id={"create"} onClick={onClickCreate}>Create</button>
-            <button id={"update"} disabled={!crudState.selectedRecord} onClick={onClickUpdate}>Update</button>
-            <button id={"delete"} disabled={!crudState.selectedRecord} onClick={onClickDelete}>Delete</button>
+            <div className="row">
+                <label htmlFor="filter">Filter prefix:</label>
+                <input aria-label={"filter"} id="filter" name="filter" type={"text"}
+                       defaultValue={crudState.filter}
+                       onChange={onChangeFilter}/>
+            </div>
+            <div className="row">
+                <div className="name-list">
+                    <select aria-label={"name-list"} id="name-list" name="name-list"
+                            size={Math.max(4, crudState?.filteredRecords.length ?? 0)}
+                            onChange={onChangeSelectNameFromCrudList}
+                    >
+                        {generateSelectOptionDataFromCrudList(crudState)}
+                    </select>
+                </div>
+                <div className="column">
+                    <div className="row">
+                        <label htmlFor="given-name">Name:</label>
+                        <input aria-label={"given-name"} id="given-name" name="given-name" type={"text"}
+                               defaultValue={givenName}
+                               onChange={onChangeGivenName}/>
+                    </div>
+                    <div className="row">
+                        <label htmlFor="surname">Surname:</label>
+                        <input aria-label={"surname"} id="surname" name="surname" type={"text"}
+                               defaultValue={surname}
+                               onChange={onChangeSurname}/>
+                    </div>
+                </div>
+            </div>
+            <div className="buttons">
+                <button id={"create"} onClick={onClickCreate}>Create</button>
+                <button id={"update"} disabled={!crudState.selectedRecord} onClick={onClickUpdate}>Update</button>
+                <button id={"delete"} disabled={!crudState.selectedRecord} onClick={onClickDelete}>Delete</button>
+            </div>
         </article>
     )
 }
